a5d6c9bc1c904a2fe9c910e6d94f4981a44f4f42
Ed Shadi <edshadi@gmail.com>
2020-02-10 14:30:26 +0200

    Adds assignments table

---
+++ migrations/sql/4_create_assignments.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `assignments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `job_id` int(10) unsigned NOT NULL,
+  `task_id` int(10) unsigned NOT NULL,
+  `worker_id` int(10) unsigned NOT NULL,
+  `response_id` int(10) unsigned,
+  `active` tinyint(1) DEFAULT '1', 
+  `assigned_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `worker_id` (`worker_id`,`job_id`, `active`, `response_id`),
+  KEY `job_id` (`job_id`),
+  KEY `task_id` (`task_id`)
+)
