312758cdd25d1a7e84bd0fb00635b9c4ceb7daa0
Slava Sobolev <sobolevslava@gmail.com>
2020-02-13 21:57:24 +0100

    fix

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -3,16 +3,16 @@ package server
 import (
 	"net/http"
 
-	"github.com/gemsorg/verification/pkg/api/assignmentcreator"
-	"github.com/gemsorg/verification/pkg/api/assignmentfetcher"
-	"github.com/gemsorg/verification/pkg/api/responsefetcher"
-	"github.com/gemsorg/verification/pkg/api/responseverifier"
-	"github.com/gemsorg/verification/pkg/authentication"
+	"github.com/expandorg/verification/pkg/api/assignmentcreator"
+	"github.com/expandorg/verification/pkg/api/assignmentfetcher"
+	"github.com/expandorg/verification/pkg/api/responsefetcher"
+	"github.com/expandorg/verification/pkg/api/responseverifier"
+	"github.com/expandorg/verification/pkg/authentication"
 
-	"github.com/gemsorg/verification/pkg/api/healthchecker"
-	"github.com/gemsorg/verification/pkg/api/settingcreator"
-	"github.com/gemsorg/verification/pkg/api/settingfetcher"
-	"github.com/gemsorg/verification/pkg/service"
+	"github.com/expandorg/verification/pkg/api/healthchecker"
+	"github.com/expandorg/verification/pkg/api/settingcreator"
+	"github.com/expandorg/verification/pkg/api/settingfetcher"
+	"github.com/expandorg/verification/pkg/service"
 	"github.com/gorilla/mux"
 )
 
@@ -25,13 +25,9 @@ func New(s service.VerificationService) http.Handler {
 	r.Handle("/verify/manual", responseverifier.MakeManualHandler(s)).Methods("POST")
 	r.Handle("/verify/automatic", responseverifier.MakeAutomaticHandler(s)).Methods("POST")
 
-<<<<<<< HEAD
 	r.Handle("/assignments", assignmentfetcher.MakeAssignmentsFetcherHandler(s)).Methods("GET")
 	r.Handle("/assignments/{assignment_id}", assignmentfetcher.MakeAssignmentFetcherHandler(s)).Methods("GET")
 
-	r.Handle("/response", responsecreator.MakeHandler(s)).Methods("POST")
-=======
->>>>>>> eda6324... consensus verification
 	r.Handle("/response", responsefetcher.MakeResponsesFetcherHandler(s)).Methods("GET")
 	r.Handle("/response/{response_id}", responsefetcher.MakeResponseFetcherHandler(s)).Methods("GET")
 	r.Handle("/settings/{job_id}", settingfetcher.MakeHandler(s)).Methods("GET")
